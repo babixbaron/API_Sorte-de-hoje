b5bbfd54be4e2e43762053c499ee66161db2c0c3
Bárbara Baron <bcostabaron@gmail.com>
2022-07-10 19:59:29 -0300

    feat: add sorteDeHoje routes

--- sorte-de-hoje/src/app.js
+++ sorte-de-hoje/src/app.js
@@ -1,13 +1,16 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
-const mongoose = require('mongoose')
+const mongoose = require('./database/mongooseConnect')
+const sorteDeHojeRoutes = require('./routes/sorteDeHojeRoutes')
 
-const app = express
+const app = express()
 
 app.use(express.json())
 app.use(cors())
 
 mongoose.connect()
+
+app.use(sorteDeHojeRoutes)
 
 module.exports = app
