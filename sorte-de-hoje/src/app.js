286e01078645e0a5f3af46f7ef22d34b3ae54edf
Bárbara Baron <bcostabaron@gmail.com>
2022-07-10 18:40:56 -0300

    chore: add dependencies on app.js

--- sorte-de-hoje/src/app.js
+++ sorte-de-hoje/src/app.js
@@ -1,7 +1,13 @@
+require('dotenv').config()
 const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
 
 const app = express
 
 app.use(express.json())
+app.use(cors())
+
+mongoose.connect()
 
 module.exports = app
