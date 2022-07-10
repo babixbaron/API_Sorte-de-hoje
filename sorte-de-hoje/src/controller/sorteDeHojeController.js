98aa30a9b7303b8d52828a5527d08830f4a7f69f
Bárbara Baron <bcostabaron@gmail.com>
2022-07-10 20:01:13 -0300

    feat: add sorteDeHojeModel and createSorte logic

--- sorte-de-hoje/src/controller/sorteDeHojeController.js
+++ sorte-de-hoje/src/controller/sorteDeHojeController.js
@@ -1,2 +1,23 @@
 const SorteDeHojeModel = require('../models/sorteDeHoje')
 
+const createSorte = async (req, res) => {
+    try {
+        const {text} = req.body
+
+        const newSorte = new SorteDeHojeModel({
+            text
+        })
+
+        const savedSorte = await newSorte.save()
+
+        res.status(201).json(savedSorte)
+
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ message: error.message})
+    }
+}
+
+module.exports = {
+    createSorte
+}
