d80883e1083d091930fd7f4e273e3bcbf77b2268
Bárbara Baron <bcostabaron@gmail.com>
2022-07-12 20:53:45 -0300

    feat: add findAllSortes route

--- src/routes/sorteDeHojeRoutes.js
+++ src/routes/sorteDeHojeRoutes.js
@@ -1,9 +1,9 @@
-const controller = require('../controller/sorteDeHojeController')
+const controller = require('../controllers/sorteDeHojeController')
 const express = require('express')
 
 const router = express.Router()
 
 router.post('/sorte', controller.createSorte)
-
+router.get('/sortes', controller.findAllSortes)
 
 module.exports = router
