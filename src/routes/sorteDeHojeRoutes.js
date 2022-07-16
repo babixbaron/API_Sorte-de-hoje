fe8e62ed79c15c735c98a7e8eea4177dab366f28
Bárbara Baron <bcostabaron@gmail.com>
2022-07-16 14:20:33 -0300

    update routes

--- src/routes/sorteDeHojeRoutes.js
+++ src/routes/sorteDeHojeRoutes.js
@@ -3,10 +3,10 @@ const express = require('express')
 
 const router = express.Router()
 
-router.post('/sorte', controller.createSorte)
+router.post('/sorte/post', controller.createSorte)
 router.get('/sortes', controller.findAllSortes)
 router.get('/sorte/:id', controller.findSorteById)
-router.patch('/sorte/:id', controller.updateSorte)
-router.delete('/sorte/:id', controller.deleteSorte)
+router.patch('/sorte/update/:id', controller.updateSorte)
+router.delete('/sorte/remove/:id', controller.deleteSorte)
 
 module.exports = router
