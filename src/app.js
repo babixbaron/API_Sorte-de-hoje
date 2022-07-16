bcbe3ec4a5ec93b28ffbf85ada8ef168e179b0eb
Bárbara Baron <bcostabaron@gmail.com>
2022-07-16 14:18:04 -0300

    install swagger dependencies

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const express = require('express')
 const cors = require('cors')
 const mongoose = require('./database/mongooseConnect')
 const sorteDeHojeRoutes = require('./routes/sorteDeHojeRoutes')
+const swaggerUi = require('swagger-ui-express')
+const swaggerFile = require('../swagger/swagger_output.json')
 
 const app = express()
 
@@ -12,5 +14,8 @@ app.use(cors())
 mongoose.connect()
 
 app.use(sorteDeHojeRoutes)
+
+
+app.use('/minha-rota-de-documentacao', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 module.exports = app
